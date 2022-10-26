2411bd714c0dbaf32139b852042c10963c512fda
francoandreDev <francoandre.dev@gmail.com>
2022-10-26 18:17:15 -0500

    end presentation

--- src/components/header/ElementBar.js
+++ src/components/header/ElementBar.js
@@ -5,15 +5,25 @@ import { setTopicId } from "../../store/slices/topicId.slice";
 const ElementBar = ({ id, text, selectedElement, setSelectedElement }) => {
     const dispatch = useDispatch();
     const liRef = useRef();
+    const checkRef = useRef();
+
     const selectLi = () => {
         setSelectedElement(id);
         liRef.current.style.color = "blue";
+        checkRef.current.id === id
+            ? (checkRef.current.checked = true)
+            : (checkRef.current.checked = false);
     };
+
     useEffect(() => {
         if (selectedElement !== id) {
             liRef.current.style.color = "";
+            if (checkRef.current.checked === true) {
+                checkRef.current.checked = false;
+            }
         }
     }, [selectedElement, id]);
+
     return (
         <li
             onClick={() => {
@@ -22,7 +32,14 @@ const ElementBar = ({ id, text, selectedElement, setSelectedElement }) => {
             }}
             ref={liRef}
         >
-            {text}
+            <h4>{text}</h4>
+
+            <input
+                type="checkbox"
+                id={id}
+                ref={checkRef}
+                style={{ opacity: 0, width: "1px", height: "1px" }}
+            />
         </li>
     );
 };
